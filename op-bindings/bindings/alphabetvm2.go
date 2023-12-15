945da2a76933d5f2bbd8a4e429cdc279d70f0e6e
Diego <105765223+0xfuturistic@users.noreply.github.com>
2023-12-15 19:41:58 +0100

    op-bindings: make

--- op-bindings/bindings/alphabetvm2.go
+++ op-bindings/bindings/alphabetvm2.go
@@ -26,6 +26,7 @@ var (
 	_ = common.Big1
 	_ = types.BloomLookup
 	_ = event.NewSubscription
+	_ = abi.ConvertType
 )
 
 // AlphabetVM2MetaData contains all meta data concerning the AlphabetVM2 contract.
@@ -156,11 +157,11 @@ func NewAlphabetVM2Filterer(address common.Address, filterer bind.ContractFilter
 
 // bindAlphabetVM2 binds a generic wrapper to an already deployed contract.
 func bindAlphabetVM2(address common.Address, caller bind.ContractCaller, transactor bind.ContractTransactor, filterer bind.ContractFilterer) (*bind.BoundContract, error) {
-	parsed, err := abi.JSON(strings.NewReader(AlphabetVM2ABI))
+	parsed, err := AlphabetVM2MetaData.GetAbi()
 	if err != nil {
 		return nil, err
 	}
-	return bind.NewBoundContract(address, parsed, caller, transactor, filterer), nil
+	return bind.NewBoundContract(address, *parsed, caller, transactor, filterer), nil
 }
 
 // Call invokes the (constant) contract method with params as input values and
--- op-bindings/bindings/outputbisectiongame.go
+++ op-bindings/bindings/outputbisectiongame.go
@@ -26,6 +26,7 @@ var (
 	_ = common.Big1
 	_ = types.BloomLookup
 	_ = event.NewSubscription
+	_ = abi.ConvertType
 )
 
 // OutputBisectionGameMetaData contains all meta data concerning the OutputBisectionGame contract.
@@ -156,11 +157,11 @@ func NewOutputBisectionGameFilterer(address common.Address, filterer bind.Contra
 
 // bindOutputBisectionGame binds a generic wrapper to an already deployed contract.
 func bindOutputBisectionGame(address common.Address, caller bind.ContractCaller, transactor bind.ContractTransactor, filterer bind.ContractFilterer) (*bind.BoundContract, error) {
-	parsed, err := abi.JSON(strings.NewReader(OutputBisectionGameABI))
+	parsed, err := OutputBisectionGameMetaData.GetAbi()
 	if err != nil {
 		return nil, err
 	}
-	return bind.NewBoundContract(address, parsed, caller, transactor, filterer), nil
+	return bind.NewBoundContract(address, *parsed, caller, transactor, filterer), nil
 }
 
 // Call invokes the (constant) contract method with params as input values and
