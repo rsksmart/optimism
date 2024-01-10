2aaa58e56f4a86fe1ea9bf07beefa1d62947d94f
Iago Lluque <iago.lluque@iovlabs.org>
2024-01-10 18:20:22 +0100

    feat: Refactored Rootstock RPC client

--- op-rsk/rsk-api/rpc.go
+++ op-rsk/rsk-api/rpc.go
@@ -8,38 +8,28 @@ import (
 	"github.com/ethereum/go-ethereum/rpc"
 )
 
-// TODO(iago) Reminder for January :D
-// I was running the setup step:
-//
-//	go run cmd/main.go genesis l2 \
-//		--deploy-config ../packages/contracts-bedrock/deploy-config/getting-started.json \
-//		--deployment-dir ../packages/contracts-bedrock/deployments/getting-started/ \
-//		--outfile.l2 genesis.json \
-//		--outfile.rollup rollup.json \
-//		--l1-rpc $L1_RPC_URL
-//
-// Plan:
-//
-//	1- be able to map from the rpc request to a RskBlock
-//	2- create the L1Block interface that holds all types.Block methods, even if returning empty for now (types.Block access is through methods, which is good for our interface usage)
-//	3- try to use L1Block directly in cmd.go
-//	4- I could not use it, not directly, the dependant classes need types.Block, which is implementing L1Block now but this does not work in Golang
-//	5- I need to change the dependant methods to receive L1Block instead of types.Block where possible and, where not, try to generate a types.Block from RskBlock
-//	6- TBD in January :)
-func GetBlockByHash(l1RPC string, blockHash common.Hash) (rsk_types.L1Block, error) {
-	client, err := rpc.Dial(l1RPC) // TODO(iago) properly configure a client
+// Client defines typed wrappers for those Rootstock RPC API specific or differing methods.
+type Client struct {
+	c *rpc.Client
+}
+
+// Dial connects a client to the given URL.
+func Dial(rawurl string) (*Client, error) {
+	client, err := rpc.Dial(rawurl)
 	if err != nil {
 		log.Error("Could not start client", err)
 		return nil, err
 	}
 
-	// Call eth_getBlockByHash RPC method
+	return &Client{c: client}, nil
+}
+
+func (ec *Client) GetBlockByHash(blockHash common.Hash) (rsk_types.L1Block, error) {
 	var header rsk_types.RskHeader
-	err = client.CallContext(context.Background(), &header, "eth_getBlockByHash", blockHash, true)
+	err := ec.c.CallContext(context.Background(), &header, "eth_getBlockByHash", blockHash, true)
 	if err != nil {
-		log.Error("Could not use client", err)
+		log.Error("Error on eth_getBlockByHash", err)
 		return nil, err
 	}
-
 	return rsk_types.NewBlock(&header), nil
 }
